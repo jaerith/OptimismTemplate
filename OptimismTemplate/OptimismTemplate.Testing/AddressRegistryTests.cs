1adc0bd1382ebe59392e71864c9ff76c6993f319
Aaron Kendall <Aaron.Kendall@gmail.com>
2021-04-11 19:07:26 -0400

    Added tests for interacting with the AddressRegistry on L2.

---
+++ OptimismTemplate/OptimismTemplate.Testing/AddressRegistryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Nethereum.ABI.Encoders;
+using Nethereum.ABI.Util;
+using Nethereum.Contracts;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using Newtonsoft.Json;
+using OptimismTemplate.Contracts.L2AddressRegistry;
+using OptimismTemplate.Contracts.L2AddressRegistry.ContractDefinition;
+using Xunit;
+
+namespace OptimismTemplate.Testing
+{
+    public class AddressRegistryTests
+    {
+
+        [Fact]
+        public async void ShouldDeployContractCreateNftMetadataUploadToIpfsAndMint()
+        {
+            var encoder = new Bytes32TypeEncoder();
+
+            var web3l2 = new Web3(new Account("0x754fde3f5e60ef2c7649061e06957c29017fe21032a8017132c0078e37f6193a", 420), "localhost:8545");
+            var ourAdddress = "0x023ffdc1530468eb8c8eebc3e38380b5bc19cc5d";
+
+            var myAddressRegistryDeployment = new L2AddressRegistryDeployment()
+            {
+                Gas = 7000000
+            };
+
+            var receipt = await L2AddressRegistryService.DeployContractAndWaitForReceiptAsync(web3l2, myAddressRegistryDeployment);
+
+            var byteCode = await web3l2.Eth.GetCode.SendRequestAsync(receipt.ContractAddress);
+
+            var service = new L2AddressRegistryService(web3l2, receipt.ContractAddress);
+
+            RegisterAddressFunction registerFunction = new RegisterAddressFunction()
+            {
+                FromAddress = "0x023ffdc1530468eb8c8eebc3e38380b5bc19cc5d",
+                ContractName = encoder.Encode(new String("AcmeCharityERC20")),
+                Gas = 7000000
+            };
+
+            var registerReceipt = await service.RegisterAddressRequestAndWaitForReceiptAsync(registerFunction);
+
+            var contractAddress = await service.GetAddressQueryAsync(encoder.Encode("AcmeCharityERC20"));
+
+            Assert.Equal("023ffdc1530468eb8c8eebc3e38380b5bc19cc5d", contractAddress);
+
+        }
+
+    }
+}
